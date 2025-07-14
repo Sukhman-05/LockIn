c2611f0d8cdffd95cdb0ef63b2ef32b60c0aba7d
Sukhman Singh <76440983+Sukhman-05@users.noreply.github.com>
2025-07-13 20:41:56 -0400

    added vercel fix

--- client/src/components/TaskPlanner.jsx
+++ client/src/components/TaskPlanner.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../AuthContext';
-import { api } from '../utils/api';
+import api from '../utils/api';
 
 const TaskPlanner = () => {
   const { user } = useAuth();
